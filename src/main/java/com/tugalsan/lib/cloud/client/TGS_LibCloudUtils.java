15d175d1965b92a2292ee85a8530a1ffb92afbd7
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-22 19:40:53 +0300

    .

--- src/main/java/com/tugalsan/lib/cloud/client/TGS_LibCloudUtils.java
+++ src/main/java/com/tugalsan/lib/cloud/client/TGS_LibCloudUtils.java
@@ -2,4 +2,6 @@ package com.tugalsan.lib.cloud.client;
 
 public class TGS_LibCloudUtils {
 
+    final public static String SERVLET_NAME_NATIVE_SUPPLY = "native_supply";
+    final public static String SERVLET_PARAM_ROW_HASH = "rowHash";
 }
